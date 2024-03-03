aa14d5959ebff8d682bf182fb81356d6615f37ad
bozhik <bozhik@playkot.com>
2024-03-03 17:02:50 +0100

    Lecture 11

---
+++ Assets/Scripts/CustomTerrain.cs
@@ -0,0 +1,54 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+[ExecuteInEditMode]
+public class CustomTerrain : MonoBehaviour
+{
+    private void Awake()
+    {
+        SerializedObject tagManager = new SerializedObject(
+            AssetDatabase.LoadAllAssetsAtPath("ProjectSettings/TagManager.asset")[0]);
+        SerializedProperty tagsProp = tagManager.FindProperty("tags");
+
+        AddTag(tagsProp, "Terrain");
+        AddTag(tagsProp, "Cloud");
+        AddTag(tagsProp, "Shore");
+
+        //apply tag changes to tag database
+        tagManager.ApplyModifiedProperties();
+
+        //take this object
+        this.gameObject.tag = "Terrain";
+    }
+
+    void AddTag(SerializedProperty tagsProp, string newTag)
+    {
+        bool found = false;
+
+        //ensure the tag doesn't already exist
+        for(int i = 0; i < tagsProp.arraySize; i++)
+        {
+            SerializedProperty t = tagsProp.GetArrayElementAtIndex(i);
+            if(t.stringValue.Equals(newTag)) { found = true; break; }
+        }
+
+        //add your new tag
+        if (!found)
+        {
+            tagsProp.InsertArrayElementAtIndex(0);
+            SerializedProperty newTagProp = tagsProp.GetArrayElementAtIndex(0);
+            newTagProp.stringValue = newTag;
+        }
+    }
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+}
